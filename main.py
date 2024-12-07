c0bea9cd74fc1556c8ef18a145fec89e9563aef5
nathanwiddi <nathanwiddi@gmail.com>
2024-12-07 14:06:10 -0800

    created player class, updated main to display player polygon

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import *
 
 def main():   
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,11 +21,11 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-
         dt = clock.tick(60) / 1000
 
 
-
+    
 if __name__ == "__main__":
     main()
